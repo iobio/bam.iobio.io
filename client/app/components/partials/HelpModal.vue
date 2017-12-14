a8dcde87db99c7cbb286781484869c72e46a0374
mjbowler <megan.j.bowler@gmail.com>
2017-12-14 12:13:57 -0700

    Update style for help modal.

--- client/app/components/partials/HelpModal.vue
+++ client/app/components/partials/HelpModal.vue
@@ -3,12 +3,34 @@
   .modal-header {
     text-align: left;
   }
+
   .modal-body {
     text-align: justify;
     padding: 30px;
     overflow: scroll;
     font-size: 12pt;
   }
+
+  .modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    display: table;
+    transition: opacity .3s ease;
+  }
+  .modal-wrapper {
+     vertical-align: top;
+   }
+
+  .modal-content {
+    width: 700px;
+    transition: all .3s ease;
+  }
+
   button.close {
     -webkit-appearance: none;
     padding: 0;
@@ -22,30 +44,33 @@
 <template>
     <transition name="modal">
       <div class="modal" style="display: block" @click="$emit('close')">
-        <div class="modal-dialog">
-          <div class="modal-content" >
+        <div class="modal-mask">
+          <div class="modal-wrapper">
+            <div class="modal-dialog">
+              <div class="modal-content" >
 
-            <div class="modal-header">
-              <slot name="header">
                 <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">{{modalTitle}}</h4>
+                  <slot name="header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">{{modalTitle}}</h4>
+                  </slot>
+                </div>
+
+                <div class="modal-body">
+                  <slot name="body">
+                    {{body}}
+                  </slot>
+                </div>
+
+                <div class="modal-footer">
+                  <slot name="footer">
+                    <button class="btn btn-default" @click.prevent="$emit('close')">
+                      Close
+                    </button>
+                  </slot>
+                </div>
+
               </div>
-              </slot>
-            </div>
-
-            <div class="modal-body">
-              <slot name="body">
-                {{body}}
-              </slot>
-            </div>
-
-            <div class="modal-footer">
-              <slot name="footer">
-                <button class="modal-default-button" @click.prevent="$emit('close')">
-                  Close
-                </button>
-              </slot>
             </div>
           </div>
         </div>
