bc832cc5cc83822a2b71931f5ab737a490e9563f
mjbowler <megan.j.bowler@gmail.com>
2018-02-08 15:49:25 -0700

    Add ability to define x and y axis label for stacked histograms.

--- client/app/components/viz/StackedHistogram.vue
+++ client/app/components/viz/StackedHistogram.vue
@@ -1,5 +1,8 @@
 <style type="text/css">
-    text { font-size: 11px; font-weight: bold;}
+    text {
+      font-size: 11px;
+      font-weight: bold;
+    }
     .iobio-bar-1 { font-size: 9px;}
     .iobio-bar-1 .instruction {
       font-size: 10px;
@@ -7,6 +10,11 @@
     rect {
       fill: #2d8fc1;
       shape-rendering: crispEdges;
+    }
+    text.axis-label-shadow {
+      stroke: white;
+      stroke-width: 3px;
+      font-weight: bold;
     }
 
 </style>
@@ -53,6 +61,14 @@ export default {
       sizeRatio: {
         default: 0.72,
         type: Number
+      },
+      xAxisLabel: {
+        default: '',
+        type: String
+      },
+      yAxisLabel: {
+        default: '',
+        type: String
       }
     },
     data() {
@@ -85,6 +101,54 @@ export default {
         },
         update: function() {
           this.draw();
+          this.addAxisLabels();
+        },
+        addAxisLabels: function() {
+          // Add labels to both axes, use a white shadow underneath the actual label to use as a background in case
+          // the label is over top of data.
+
+          // Select the axis text element, if it exists.
+          var svgx = d3.select(this.$el).select("svg").selectAll('.x.axis-label').data([0]);
+          var svgy = d3.select(this.$el).select("svg").selectAll('.y.axis-label').data([0]);
+
+          // Otherwise, create axis text element.
+          var xsEnter = svgx.enter().append('text').attr('class', 'x axis-label-shadow');
+          var xEnter = svgx.enter().append('text').attr('class', 'x axis-label');
+          var ysEnter = svgy.enter().append('text').attr('class', 'y axis-label-shadow');
+          var yEnter = svgy.enter().append('text').attr('class', 'y axis-label');
+
+          // Label positions
+          var xLabelX = $(this.$el).width()/2;//$("#read-coverage-distribution-chart").width() ; //7*this.width/12;
+          var xLabelY = this.height * this.sizeRatio - 26;
+          var yLabelX = - 5 * this.height * this.sizeRatio / 12;
+          var yLabelY = 55;
+
+          // X axis
+          d3.select(this.$el).select('.x.axis-label-shadow')
+            .attr("text-anchor", "middle")
+            .attr("x", xLabelX)
+            .attr("y", xLabelY)
+            .text(this.xAxisLabel);
+          d3.select(this.$el).select('.x.axis-label')
+            .attr("text-anchor", "middle")
+            .attr("x", xLabelX)
+            .attr("y", xLabelY)
+            .text(this.xAxisLabel);
+          // Y axis
+          d3.select(this.$el).select('.y.axis-label-shadow')
+            .attr("text-anchor", "middle")
+            .attr("y", yLabelY)
+            .attr("x",  yLabelX)
+            .attr("dy", ".75em")
+            .attr("transform", "rotate(-90)")
+            .text(this.yAxisLabel);
+          d3.select(this.$el).select('.y.axis-label')
+            .attr("text-anchor", "middle")
+            .attr("y", yLabelY)
+            .attr("x",  yLabelX)
+            .attr("dy", ".75em")
+            .attr("transform", "rotate(-90)")
+            .text(this.yAxisLabel);
         }
     },
     watch: {
