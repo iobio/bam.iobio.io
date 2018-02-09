d5423a24e37714418863a22a1e25a8454a53a49b
mjbowler <megan.j.bowler@gmail.com>
2018-02-09 13:36:03 -0700

    History mode so old and new URLs will work and no #'s.

--- client/app/routes.js
+++ client/app/routes.js
@@ -50,7 +50,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  // mode: 'history',
+  mode: 'history',
   routes: routes
 })
 
--- server/express-app.js
+++ server/express-app.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3001;
 var path = require('path');
+var history = require('connect-history-api-fallback');
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
-app.get('/', function(req, res){
+app.use(history());
+
+app.get('/index.html', function(req, res){
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
