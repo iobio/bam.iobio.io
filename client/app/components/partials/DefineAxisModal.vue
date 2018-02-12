ddb21bc7d6cc3dbb80b3ec939c1582051c56addb
mjbowler <megan.j.bowler@gmail.com>
2018-02-12 16:46:21 -0700

    Add ability to define custom ranges for axes for histograms.

---
+++ client/app/components/partials/DefineAxisModal.vue
@@ -0,0 +1,156 @@
+<style type="text/css">
+
+  .modal {
+    transition: all .3s ease;
+  }
+
+  .modal-header {
+    text-align: left;
+  }
+
+  .modal-body {
+    text-align: justify;
+    padding: 30px;
+    overflow: auto;
+    font-size: 11pt;
+    max-height: 500px;
+  }
+
+  .modal-body .row {
+    margin-top: 30px;
+  }
+
+  .modal-mask {
+    position: absolute;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    display: table;
+    transition: opacity .3s ease;
+  }
+  .modal-wrapper {
+     vertical-align: top;
+   }
+
+  .modal-content {
+    width: 700px;
+    transition: all .3s ease;
+  }
+
+  button.close {
+    -webkit-appearance: none;
+    padding: 0;
+    cursor: pointer;
+    background: transparent;
+    border: 0;
+  }
+
+  form label {
+    width: 60px;
+  }
+  input {
+    width: 300px;
+  }
+
+</style>
+
+<template>
+    <transition name="modal">
+      <div class="modal" style="display: block" @click="$emit('close')">
+        <div class="modal-mask">
+          <div class="modal-wrapper" @click.stop>
+            <div class="modal-dialog">
+              <div class="modal-content" >
+
+                <div class="modal-header">
+                  <slot name="header">
+                    <button type="button" class="close" @click="$emit('close')">&times;</button>
+                    <h4 class="modal-title">Define custom ranges for axes:</h4>
+                  </slot>
+                </div>
+
+                <div class="modal-body">
+                  <slot name="body">
+                    <form>
+                      <div class="form-group">
+                        <label class="form-control-label" for="xMin">xMin</label>
+                        <input id="xMin" v-model="xMin" placeholder="Enter a custom lower bound for the x axis.">
+                      </div><!-- /form-group -->
+                      <div class="form-group">
+                        <label class="form-control-label" for="xMax">xMax</label>
+                        <input id="xMax" v-model="xMax" placeholder="Enter a custom upper bound for the x axis.">
+                      </div><!-- /form-group -->
+                      <div class="form-group">
+                        <label class="form-control-label" for="yMin">yMin</label>
+                        <input id="yMin" v-model="yMin" placeholder="Enter a custom lower bound for the y axis.">
+                      </div><!-- /form-group -->
+                      <div class="form-group">
+                        <label class="form-control-label" for="yMax">xMin</label>
+                        <input id="yMax" v-model="yMax" placeholder="Enter a custom upper bound for the y axis.">
+                      </div><!-- /form-group -->
+                    </form>
+                  </slot>
+                </div>
+
+                <div class="modal-footer">
+                  <slot name="footer">
+                    <button class="btn btn-default" @click.prevent="reset">
+                      Reset
+                    </button>
+                    <button class="btn btn-default" @click.prevent="save">
+                      Save
+                    </button>
+                  </slot>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+
+</template>
+
+<script>
+export default {
+  name: 'define-axis-modal',
+  props: {
+    xMinOrig: {},
+    xMaxOrig: {},
+    yMinOrig: {},
+    yMaxOrig: {},
+  },
+  data() {
+    return {
+      showModal: false,
+      xMin: "",
+      xMax: "",
+      yMin: "",
+      yMax: ""
+    }
+  },
+  methods: {
+    reset: function() {
+      this.xMin = "";
+      this.xMax = "";
+      this.yMin = "";
+      this.yMax = "";
+    },
+    save: function() {
+      this.$emit('updateAxesRanges',this.xMin,this.xMax,this.yMin,this.yMax);
+    }
+  },
+  created: function () {
+    this.xMin = this.xMinOrig;
+    this.xMax = this.xMaxOrig;
+    this.yMin = this.yMinOrig;
+    this.yMax = this.yMinOrig;
+  },
+}
+
+
+</script>
--- client/app/components/viz/StackedHistogram.vue
+++ client/app/components/viz/StackedHistogram.vue
@@ -23,19 +23,37 @@
       stroke-width: 5px;
       font-weight: bold;
     }
+    .noselect {
+      -webkit-touch-callout: none; /* iOS Safari */
+      -webkit-user-select: none; /* Safari */
+      -khtml-user-select: none; /* Konqueror HTML */
+      -moz-user-select: none; /* Firefox */
+      -ms-user-select: none; /* Internet Explorer/Edge */
+      user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+    }
 
 </style>
 
 <template>
     <div>
-
+      <define-axis-modal v-if='showAxisModal'
+                         :xMinOrig="xMin"
+                         :xMaxOrig="xMax"
+                         :yMinOrig="yMin"
+                         :yMaxOrig="yMax"
+                         @updateAxesRanges="updateAxesRanges"
+                         @close='showAxisModal = false'></define-axis-modal>
     </div>
 </template>
 
 <script>
 
+import DefineAxisModal from "../partials/DefineAxisModal.vue";
+
 export default {
-    name: 'stacked-histogram',
+  components: {DefineAxisModal},
+  name: 'stacked-histogram',
     props: {
       data: {},
       width: {
@@ -81,6 +99,11 @@ export default {
     data() {
       return {
         histogramChart: {},
+        xMin: "",
+        xMax: "",
+        yMin: "",
+        yMax: "",
+        showAxisModal: false
       }
     },
     created: function() {
@@ -104,31 +127,39 @@ export default {
     methods: {
       draw: function() {
         var selection = d3.select(this.$el).datum(this.data);
-        this.histogramChart(selection);
+        this.histogramChart(selection, this.getOptions());
+      },
+      getOptions: function() {
+        var options = {};
+        if (isNumeric(this.xMin)) options.xMin = this.xMin;
+        if (isNumeric(this.xMax)) options.xMax = this.xMax;
+        if (isNumeric(this.yMin)) options.yMin = this.yMin;
+        if (isNumeric(this.yMax)) options.yMax = this.yMax;
+
+        return options;
       },
       update: function() {
         this.draw();
         this.addAxisLabels();
       },
+      updateAxesRanges: function(xmin, xmax, ymin, ymax) {
+        this.showAxisModal = false;
+        this.xMin = isNumeric(xmin) ? xmin : "";
+        this.xMax = isNumeric(xmax) ? xmax : "";
+        this.yMin = isNumeric(ymin) ? ymin : "";
+        this.yMax = isNumeric(ymax) ? ymax : "";
+        this.draw();
+      },
       addAxisLabels: function() {
 
+        // X axis and zoom hint
         // Select x axis and zoom hint divs if they exist.
         var zoomHintDiv = d3.select(this.$el).select(".iobio-bar-1").selectAll('.zoom-label').data([0]);
         var xAxisLabelDiv = d3.select(this.$el).select(".iobio-bar-1").selectAll('.axis-label').data([0]);
         // Otherwise, create the divs.
-        var zoomHintEnter = zoomHintDiv.enter().append('div').attr('class', 'zoom-label');
-        var xAxisEnter = xAxisLabelDiv.enter().append('div').attr('class', 'x axis-label');
+        var zoomHintEnter = zoomHintDiv.enter().append('div').attr('class', 'zoom-label noselect');
+        var xAxisEnter = xAxisLabelDiv.enter().append('div').attr('class', 'x axis-label noselect');
 
-        // Select the y axis label text element, if it exists.
-        var chartSVG = d3.select(this.$el).select("svg").selectAll('.y.axis-label').data([0]);
-        // Otherwise, create it.
-        var yAxisEnter = chartSVG.enter().append('text').attr('class', 'y axis-label');
-
-        // Y axis label positions
-        var yLabelX = - 5 * this.height * this.sizeRatio / 12;
-        var yLabelY = 6;
-
-        // X axis and zoom hint
         d3.select(this.$el).select('.zoom-label')
           .style("float", "right")
           .style("padding-right","15px")
@@ -137,16 +168,42 @@ export default {
           .style("text-align", "center")
           .style("margin", "0 auto")
           .style("width", "100px")
+          .style("cursor","pointer")
+          .attr("title", "Double click to define custom axis ranges.")
           .text(this.xAxisLabel);
 
         // Y Axis
+        // Select the y axis label text element, if it exists.
+        var chartSVG = d3.select(this.$el).select('svg').selectAll('.y.axis-label').data([0]);
+        // Otherwise, create it.
+        var yAxisEnter = chartSVG.enter().append('text').attr('class', 'y axis-label noselect');
+
+        // Y axis label positions
+        var yLabelX = - 5 * this.height * this.sizeRatio / 12;
+        var yLabelY = 6;
+
         d3.select(this.$el).select('.y.axis-label')
           .attr("text-anchor", "middle")
           .attr("y", yLabelY)
           .attr("x",  yLabelX)
           .attr("dy", ".75em")
           .attr("transform", "rotate(-90)")
+          .attr("title", "Double click to define custom axis ranges.")
+          .style("cursor","pointer")
           .text(this.yAxisLabel);
+
+        // Tooltip for y axis label
+        var ttElement = d3.select(this.$el).select('svg').select('.y.axis-label').selectAll('.title').data([0]);
+        var yAxisTTEnter = ttElement.enter().append('title').text("Double click to define custom axis ranges.");
+
+        // Add listeners to axis labels
+        d3.select(this.$el).select('.x.axis-label').on("dblclick", function() {
+          this.showAxisModal = true;
+        }.bind(this))
+
+        d3.select(this.$el).select('.y.axis-label').on("dblclick", function() {
+          this.showAxisModal = true;
+        }.bind(this))
 
       }
     },
@@ -174,6 +231,10 @@ function tickFormatter (d) {
 function precisionRound(number, precision) {
   var factor = Math.pow(10, precision);
   return Math.round(number * factor) / factor;
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 </script>
