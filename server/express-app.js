c9432f91b185611d509d8751360a20c4755ab004
Anders Pitman <tapitman11@gmail.com>
2019-04-12 17:37:26 -0600

    Enable static serving
    Needing to move index.html to be relative to the other files
    it includes. The only reason the node server is still necessary
    is for properly make the frontend handle routing. But services
    like AWS can do this for us in production.

--- server/express-app.js
+++ server/express-app.js
@@ -5,11 +5,6 @@ var path = require('path');
 var history = require('connect-history-api-fallback');
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
-
 app.use(history());
-
-app.get('/index.html', function(req, res){
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
 
 module.exports = app;
