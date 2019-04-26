16d7bf411f753118e5b42b21605d11a894d023ef
Anders Pitman <tapitman11@gmail.com>
2019-04-26 16:57:11 -0600

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 # build vue app
 if [[ $1 == "prod" ]]; then
   echo "** Building prod **"
-  NODE_ENV=production npm run build
+  #NODE_ENV=production npm run build
+  npm run build
 elif [[ $1 == "stage" ]]; then
   echo "** Building stage **"
   npm run build
@@ -13,15 +14,17 @@ else
 fi
 
 
-
-working_dir=$PWD
-
 # upload to cloudfront
 if [[ $1 == "stage" ]]; then
 
-  echo "** Uploaded to stage s3 bucket **"
+  echo "** Uploading to stage s3 bucket **"
   aws s3 cp . s3://static.iobio.io/stage/bam.iobio.io/ --recursive --exclude '.git/*'
   echo "** Renew cloudfrount cache **"
   aws cloudfront create-invalidation --distribution-id EW1T1HDKHSTBF --paths /\*
 
+elif [[ $1 == "prod" ]]; then
+  echo "** Uploading to prod s3 bucket **"
+  aws s3 cp ./client s3://static.iobio.io/prod/bam.iobio.io/green/ --recursive --cache-control 'public, max-age=86400'
+  echo "** Renew cloudfrount cache **"
+  aws cloudfront create-invalidation --distribution-id E1SI8J5TK5FF8 --paths /\*
 fi
